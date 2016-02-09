32bfc376e48bafdfec4e932623903c1f3fda703d
Rúben A.M. Cruz <rubenamcruz@gmail.com>
2016-02-09 15:59:04 +0000

    added data.js

---
+++ data.js
@@ -0,0 +1,15 @@
+
+var dados = [['Work',     11],
+['Eat',      2],
+['Commute',  2],
+['Watch TV', 2],
+['Sleep',    7]
+]
+
+var option = {
+  title: 'My Daily Activities',
+  pieSliceText: 'percentage',
+};
+
+
+pieChart(dados, option);
